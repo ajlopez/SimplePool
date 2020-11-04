8e8a453fa7ec1708fd8b3539f53fe0c90083517f
ajlopez <webmaster@ajlopez.com>
2020-11-04 11:07:06 -0300

    Refactor test

--- test/LiquidityPool_test.js
+++ test/LiquidityPool_test.js
@@ -2,14 +2,21 @@
 const LiquidityPool = artifacts.require('LiquidityPool');
 
 contract('LiquidityPool', function (accounts) {   
+    let liquidityPool;
+    
+    const root = accounts[0];
+    const token = accounts[1];
+    
+    beforeEach(async function () {
+        liquidityPool = await LiquidityPool.new(token);
+    });
+    
     it('create contract', async function () {
-        const liquidityPool = await LiquidityPool.new(accounts[0]);
-        
         const token = await liquidityPool.token();
         const tokenBalance = Number(await liquidityPool.tokenBalance());
         const cryptoBalance = Number(await liquidityPool.cryptoBalance());
         
-        assert.equal(token, accounts[0]);
+        assert.equal(token, accounts[1]);
         assert.equal(tokenBalance, 0);
         assert.equal(cryptoBalance, 0);
     });
