e9e07194e99791e9ecdb15397423360e71ca0733
ajlopez <webmaster@ajlopez.com>
2020-11-12 15:15:11 -0300

    Refactor LiquidityPool tests

--- README.md
+++ README.md
@@ -6,12 +6,15 @@ and Automated Market Maker Algorithm
 ## References
 
 - [Uniswap Exchange Protocol](https://uniswap.io/)
+- [Uniswap V2 Documentation](https://uniswap.org/docs/v2)
 - [What is the Uniswap Protocol Token (UNI)?](https://research.binance.com/en/projects/uniswap)
 - [Uniswap’s Distribution Is Built on Something That Can’t Be Forked: Actual Users](https://www.coindesk.com/uniswap-uni-distribution-growth-token-defi-strategy)
 - [UNI defies price dump predictions, emerges as top 20 DeFi token](https://cointelegraph.com/news/uni-defies-price-dump-predictions-emerges-as-top-20-defi-token)
 - [Understanding Uniswap: how to become a successful Liquidity provider and earn trading feed](https://cryptotesters.com/tutorial/understanding-uniswap)
 - [Uniswap Rises to Top of DeFi Charts Thanks to Rival Looking to Unseat It](https://www.coindesk.com/uniswap-defi-pulse-tvl-leader-sushiswap)
 - [A Deep Dive into Liquidity Pools](https://blog.zerion.io/liquidity-pools-8ac8cf8cf230)
+- [Uniswap: A Good Deal for Liquidity Providers?](https://pintail.medium.com/uniswap-a-good-deal-for-liquidity-providers-104c0b6816f2) with sample of adding liquidity
+- [Understanding Uniswap Returns](https://pintail.medium.com/understanding-uniswap-returns-cc593f3499ef)
 
 ## License
 
--- test/LiquidityPool_test.js
+++ test/LiquidityPool_test.js
@@ -10,80 +10,111 @@ contract('LiquidityPool', function (accounts) {
     const alice = accounts[1];
     const bob = accounts[2];
     
-    beforeEach(async function () {
-        token = await BasicToken.new('Token', 'TOK');
-        await token.transfer(alice, 1000000);
-        liquidityPool = await LiquidityPool.new(token.address);
+    describe('deposits', function () {
+        beforeEach(async function () {
+            token = await BasicToken.new('Token', 'TOK');
+            await token.transfer(alice, 1000000);
+            liquidityPool = await LiquidityPool.new(token.address);
+        });
+        
+        it('initial state', async function () {
+            const poolToken = await liquidityPool.token();
+            const tokenBalance = Number(await liquidityPool.tokenBalance());
+            const cryptoBalance = Number(await liquidityPool.cryptoBalance());
+            
+            assert.equal(poolToken, token.address);
+            assert.equal(tokenBalance, 0);
+            assert.equal(cryptoBalance, 0);
+        });
+        
+        it('deposit funds', async function () {
+            const initialAliceTokenBalance = Number(await token.balanceOf(alice));
+            const initialAliceBalance = Number(await web3.eth.getBalance(alice));
+            const initialPoolTokenBalance = Number(await token.balanceOf(liquidityPool.address));
+            const initialPoolBalance = Number(await web3.eth.getBalance(liquidityPool.address));
+            
+            await token.approve(liquidityPool.address, 1000, { from: alice, gasPrice: 0 });
+            await liquidityPool.deposit(1000, { value: 1000000, from: alice, gasPrice: 0 });
+
+            const finalAliceTokenBalance = Number(await token.balanceOf(alice));
+            const finalAliceBalance = Number(await web3.eth.getBalance(alice));
+            const finalPoolTokenBalance = Number(await token.balanceOf(liquidityPool.address));
+            const finalPoolBalance = Number(await web3.eth.getBalance(liquidityPool.address));
+            
+            assert.equal(finalAliceBalance, initialAliceBalance - 1000000);
+            assert.equal(finalAliceTokenBalance, initialAliceTokenBalance - 1000);
+            assert.equal(finalPoolBalance, initialPoolBalance + 1000000);
+            assert.equal(finalPoolTokenBalance, initialPoolTokenBalance + 1000);
+        });
+        
+        it('two deposits', async function () {
+            const initialAliceTokenBalance = Number(await token.balanceOf(alice));
+            const initialAliceBalance = Number(await web3.eth.getBalance(alice));
+            const initialPoolTokenBalance = Number(await token.balanceOf(liquidityPool.address));
+            const initialPoolBalance = Number(await web3.eth.getBalance(liquidityPool.address));
+            
+            await token.approve(liquidityPool.address, 1000, { from: alice, gasPrice: 0 });
+            await liquidityPool.deposit(1000, { value: 1000000, from: alice, gasPrice: 0 });
+            await token.approve(liquidityPool.address, 100, { from: alice, gasPrice: 0 });
+            await liquidityPool.deposit(100, { value: 100000, from: alice, gasPrice: 0 });
+
+            const finalAliceTokenBalance = Number(await token.balanceOf(alice));
+            const finalAliceBalance = Number(await web3.eth.getBalance(alice));
+            const finalPoolTokenBalance = Number(await token.balanceOf(liquidityPool.address));
+            const finalPoolBalance = Number(await web3.eth.getBalance(liquidityPool.address));
+            
+            assert.equal(finalAliceBalance, initialAliceBalance - 1100000);
+            assert.equal(finalAliceTokenBalance, initialAliceTokenBalance - 1100);
+            assert.equal(finalPoolBalance, initialPoolBalance + 1100000);
+            assert.equal(finalPoolTokenBalance, initialPoolTokenBalance + 1100);
+        });
     });
     
-    it('create contract', async function () {
-        const poolToken = await liquidityPool.token();
-        const tokenBalance = Number(await liquidityPool.tokenBalance());
-        const cryptoBalance = Number(await liquidityPool.cryptoBalance());
-        
-        assert.equal(poolToken, token.address);
-        assert.equal(tokenBalance, 0);
-        assert.equal(cryptoBalance, 0);
-    });
-    
-    it('first funds', async function () {
-        const initialAliceTokenBalance = Number(await token.balanceOf(alice));
-        const initialAliceBalance = Number(await web3.eth.getBalance(alice));
-        const initialPoolTokenBalance = Number(await token.balanceOf(liquidityPool.address));
-        const initialPoolBalance = Number(await web3.eth.getBalance(liquidityPool.address));
-        
-        await token.approve(liquidityPool.address, 1000, { from: alice, gasPrice: 0 });
-        await liquidityPool.deposit(1000, { value: 1000000, from: alice, gasPrice: 0 });
 
-        const finalAliceTokenBalance = Number(await token.balanceOf(alice));
-        const finalAliceBalance = Number(await web3.eth.getBalance(alice));
-        const finalPoolTokenBalance = Number(await token.balanceOf(liquidityPool.address));
-        const finalPoolBalance = Number(await web3.eth.getBalance(liquidityPool.address));
+    describe('buy and sell', function () {
+        beforeEach(async function () {
+            token = await BasicToken.new('Token', 'TOK');
+            await token.transfer(alice, 1000000);
+            liquidityPool = await LiquidityPool.new(token.address);
+            
+            await token.approve(liquidityPool.address, 1000, { from: alice, gasPrice: 0 });
+            await liquidityPool.deposit(1000, { value: 1000000, from: alice, gasPrice: 0 });
+        });
         
-        assert.equal(finalAliceBalance, initialAliceBalance - 1000000);
-        assert.equal(finalAliceTokenBalance, initialAliceTokenBalance - 1000);
-        assert.equal(finalPoolBalance, initialPoolBalance + 1000000);
-        assert.equal(finalPoolTokenBalance, initialPoolTokenBalance + 1000);
-    });
-    
-    it('buy tokens', async function () {
-        await token.approve(liquidityPool.address, 1000, { from: alice, gasPrice: 0 });
-        await liquidityPool.deposit(1000, { value: 1000000, from: alice, gasPrice: 0 });
+        it('buy tokens', async function () {
+            await liquidityPool.buyTokens({ from: bob, value: 1000000 });
+            
+            const bobTokens = Number(await token.balanceOf(bob));
+            const cryptoBalance = Number(await liquidityPool.cryptoBalance());
+            const tokenBalance = Number(await liquidityPool.tokenBalance());
+            const poolTokens = Number(await token.balanceOf(liquidityPool.address));
+            const poolBalance = Number(await web3.eth.getBalance(liquidityPool.address));
 
-        await liquidityPool.buyTokens({ from: bob, value: 1000000 });
+            assert.equal(bobTokens, 500);
+            assert.equal(poolTokens, 500);
+            assert.equal(poolBalance, 2000000);
+            assert.equal(cryptoBalance, 2000000);
+            assert.equal(tokenBalance, 500);
+        });
         
-        const bobTokens = Number(await token.balanceOf(bob));
-        const cryptoBalance = Number(await liquidityPool.cryptoBalance());
-        const tokenBalance = Number(await liquidityPool.tokenBalance());
-        const poolTokens = Number(await token.balanceOf(liquidityPool.address));
-        const poolBalance = Number(await web3.eth.getBalance(liquidityPool.address));
+        it('sell tokens', async function () {
+            await token.transfer(bob, 1000000);
 
-        assert.equal(bobTokens, 500);
-        assert.equal(poolTokens, 500);
-        assert.equal(poolBalance, 2000000);
-        assert.equal(cryptoBalance, 2000000);
-        assert.equal(tokenBalance, 500);
-    });
-    
-    it('sell tokens', async function () {
-        await token.transfer(bob, 1000000);
-        await token.approve(liquidityPool.address, 1000, { from: alice, gasPrice: 0 });
-        await liquidityPool.deposit(1000, { value: 1000000, from: alice, gasPrice: 0 });
+            await token.approve(liquidityPool.address, 1000, { from: bob, gasPrice: 0 });
+            await liquidityPool.sellTokens(1000, { from: bob });
+            
+            const bobTokens = Number(await token.balanceOf(bob));
+            const cryptoBalance = Number(await liquidityPool.cryptoBalance());
+            const tokenBalance = Number(await liquidityPool.tokenBalance());
+            const poolTokens = Number(await token.balanceOf(liquidityPool.address));
+            const poolBalance = Number(await web3.eth.getBalance(liquidityPool.address));
 
-        await token.approve(liquidityPool.address, 1000, { from: bob, gasPrice: 0 });
-        await liquidityPool.sellTokens(1000, { from: bob });
-        
-        const bobTokens = Number(await token.balanceOf(bob));
-        const cryptoBalance = Number(await liquidityPool.cryptoBalance());
-        const tokenBalance = Number(await liquidityPool.tokenBalance());
-        const poolTokens = Number(await token.balanceOf(liquidityPool.address));
-        const poolBalance = Number(await web3.eth.getBalance(liquidityPool.address));
-
-        assert.equal(bobTokens, 1000000 - 1000);
-        assert.equal(poolTokens, 2000);
-        assert.equal(poolBalance, 500000);
-        assert.equal(cryptoBalance, 500000);
-        assert.equal(tokenBalance, 2000);
+            assert.equal(bobTokens, 1000000 - 1000);
+            assert.equal(poolTokens, 2000);
+            assert.equal(poolBalance, 500000);
+            assert.equal(cryptoBalance, 500000);
+            assert.equal(tokenBalance, 2000);
+        });
     });
 });
 
