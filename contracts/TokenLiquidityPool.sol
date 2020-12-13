b7fce04c3331a95343e98c224743706a02fb6615
ajlopez <webmaster@ajlopez.com>
2020-12-13 08:36:26 -0300

    Token liquidity pool first deposit

--- contracts/TokenLiquidityPool.sol
+++ contracts/TokenLiquidityPool.sol
@@ -5,11 +5,22 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 contract TokenLiquidityPool {
     ERC20 public token1;
     ERC20 public token2;
+
+    uint256 public token1Balance;
+    uint256 public token2Balance;
     
     constructor(ERC20 _token1, ERC20 _token2) public {
         token1 = _token1;
         token2 = _token2;
-    }    
+    }
+    
+    function deposit(uint256 token1Amount, uint256 token2Amount) public payable {
+        require(token1.transferFrom(msg.sender, address(this), token1Amount));
+        require(token2.transferFrom(msg.sender, address(this), token2Amount));
+        
+        token1Balance += token1Amount;
+        token2Balance += token2Amount;
+    }
 }
 
 
--- test/TokenLiquidityPool_test.js
+++ test/TokenLiquidityPool_test.js
@@ -2,6 +2,10 @@ const TokenLiquidityPool = artifacts.require('TokenLiquidityPool');
 const BasicToken = artifacts.require('BasicToken');
 
 contract('TokenLiquidityPool', function (accounts) {   
+    const alice = accounts[0];
+    const bob = accounts[1];
+    const charlie = accounts[2];
+    
     it('creation', async function() {
         const token1 = await BasicToken.new('Token 1', 'TOK1');
         const token2 = await BasicToken.new('Token 2', 'TOK2');
@@ -10,6 +14,39 @@ contract('TokenLiquidityPool', function (accounts) {
 
         assert.equal(await tokenLiquidityPool.token1(), token1.address);
         assert.equal(await tokenLiquidityPool.token2(), token2.address);
+    });
+    
+    it('first deposit', async function() {
+        const token1 = await BasicToken.new('Token 1', 'TOK1');
+        const token2 = await BasicToken.new('Token 2', 'TOK2');
+        
+        await token1.transfer(alice, 1000000);
+        await token2.transfer(alice, 1000000);
+        
+        const tokenLiquidityPool = await TokenLiquidityPool.new(token1.address, token2.address);
+        
+        const initialAliceToken1Balance = Number(await token1.balanceOf(alice));
+        const initialPoolToken1Balance = Number(await token1.balanceOf(tokenLiquidityPool.address));
+        const initialAliceToken2Balance = Number(await token2.balanceOf(alice));
+        const initialPoolToken2Balance = Number(await token2.balanceOf(tokenLiquidityPool.address));
+        
+        await token1.approve(tokenLiquidityPool.address, 1000, { from: alice});
+        await token2.approve(tokenLiquidityPool.address, 500, { from: alice});
+
+        await tokenLiquidityPool.deposit(1000, 500);
+
+        const finalAliceToken1Balance = Number(await token1.balanceOf(alice));
+        const finalPoolToken1Balance = Number(await token1.balanceOf(tokenLiquidityPool.address));
+        const finalAliceToken2Balance = Number(await token2.balanceOf(alice));
+        const finalPoolToken2Balance = Number(await token2.balanceOf(tokenLiquidityPool.address));
+        
+        assert.equal(finalAliceToken1Balance, initialAliceToken1Balance - 1000);
+        assert.equal(finalAliceToken2Balance, initialAliceToken2Balance - 500);
+        assert.equal(finalPoolToken1Balance, initialPoolToken1Balance + 1000);
+        assert.equal(finalPoolToken2Balance, initialPoolToken2Balance + 500);
+        
+        assert.equal(Number(await tokenLiquidityPool.token1Balance()), 1000);
+        assert.equal(Number(await tokenLiquidityPool.token2Balance()), 500);
     });
 });
 
